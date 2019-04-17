da2e0bb536032b637909324050a352d36ab1006c
Helfstein <Helfstein-Note>
2019-04-17 16:58:11 -0300

    testing 5

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-var express = require('express');
-
-var app = express()
-  .use(express.static(__dirname + '/index.html'))
-  .listen(process.env.PORT || 5000);
+var static = require('node-static');
+var file = new static.Server();
+require('http').createServer(function(request, response) {
+  request.addListener('end', function() {
+    file.serve(request, response);
+  }).resume();
+}).listen(process.env.PORT || 3000);
