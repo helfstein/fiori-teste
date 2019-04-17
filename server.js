1fe0cea24ec57fabba0c53f0185f908e53779269
Helfstein <Helfstein-Note>
2019-04-17 17:07:39 -0300

    testing 6

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-var static = require('node-static');
-var file = new static.Server();
-require('http').createServer(function(request, response) {
-  request.addListener('end', function() {
-    file.serve(request, response);
-  }).resume();
-}).listen(process.env.PORT || 3000);
+var express = require('express');
+
+var express = require('express');
+
+var server = express(); 
+server.configure(function(){
+  //server.use('/media', express.static(__dirname + '/media'));
+  server.use(express.static(__dirname + '/public'));
+});
+
+server.listen(3000);
